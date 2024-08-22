8d75d2db4d52392e94f6fb775c8308513d6659fe
Rafael Gomes <maciel.rafael1@gmail.com>
2024-08-22 16:56:23 -0400

    update server

---
+++ server/api/index.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const helmet = require("helmet");
+const taskRoutes = require("./routes/tasks");
+const errorHandler = require("./middleware/errorHandler");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: ["https://task-management-backend-lovat.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+app.use(helmet());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGODB_URI, {})
+  .then(() => console.log("Database Connected!"))
+  .catch((err) => console.error("Database connection error:", err));
+
+app.use("/tasks", taskRoutes);
+
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
